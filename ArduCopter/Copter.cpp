24f02e9b5f9ab1653c625d9527a92f869af80120
leiyihang <2877241385@qq.com>
2026-04-30 16:22:39 +0800

    change copter

--- ArduCopter/Copter.cpp
+++ ArduCopter/Copter.cpp
@@ -270,12 +270,8 @@ void Copter::update_router()
 {
     // 获取当前的 MAVLink 系统 ID
     uint8_t my_sysid = gcs().sysid_this_mav();
-<<<<<<< HEAD
     if(my_sysid != 1) return;
-=======
     GCS_SEND_TEXT(MAV_SEVERITY_WARNING, "Now flight mode: %d", AP::vehicle()->get_mode());
-    if(my_sysid != 1) return;  // 如果不是主无人机，不进行路由更新
->>>>>>> f50c20990ae30224e636f0697022b8bab9fdb8b2
     for (int i = 0; i < 4; i++) {
         uint8_t maybe_new_son = net_zero_router.backup_son[i];
         if (maybe_new_son == 0xFF) {
